1680bef68b026a3ba0f0d3ae2e94fa549d9f3637
Lindsay Spencer <lindsaymspencer@outlook.com>
2020-09-20 12:35:20 -0500

    Built out Officer Model to include CwoPqs/Sectionss.

--- CwoPqsApp/Models/Officer.cs
+++ CwoPqsApp/Models/Officer.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CwoPqsApp.Services;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CwoPqsApp.Models
 {
     public class Officer
     {
+        [JsonIgnore]
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Rank { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = "Lindsay";
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = "Spencer";
+
+        public string Rank { get; set; } = "LTJG";
+
+        [NotMapped]
+        public CwoPqs TwoBravo { get; set; } = CwoPqs.NewTwoBravo();
+
+        [JsonIgnore]
+        [NotMapped]
+        public CwoPqs TwoAlpha { get; set; } = CwoPqs.NewTwoAlpha();
+
+        public string ToJson() => JsonSerializer.Serialize(this);
+
+        public bool Export(string path)
+        {
+            FileHelper.WriteOver(Path.Combine(path, LastName + FirstName + ".cw2bpqs"),
+                ToJson());
+            return true;
+        }
+
     }
 }
---
+++ CwoPqsApp/Models/Section.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CwoPqsApp.Models
+{
+    public class Section
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        [NotMapped]
+        public Dictionary<string, bool> Signatures { get; set; }
+    }
+}
